fb694a16ddccabdac4cb2a4f17d6d30fd8d3cc7a
rakugaki0cam <isober21@gmail.com>
2022-08-28 16:15:02 +0900

    new

---
+++ rk4.py
@@ -0,0 +1,78 @@
+# 4次ルンゲ・クッタ法
+#
+#
+# sikinote(https://slpr.sakura.ne/jp/qp) 2020/04/22によります
+#
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def grk(x, y):
+    #計算する関数
+    y = -y * math.sin(x)
+    return x, y
+
+def rk4(x, y, n, h):
+    #4次ルンゲクッタ法によって解を求める
+    # x  --> 変数
+    # y  --> 解
+    # n  --> 方程式数（1階微分方程式:n=1,連立1階微分方程式:n=2)
+    # h  --> xの増加ステップ
+    
+    c = (0, 0.5, 0.5, 1)    #4次
+    k = []
+    f = []
+    tmp = []
+    
+    for i in range(n):
+        k.append([0, 0, 0, 0])
+        f.append(0)
+        tmp.append(0)
+
+    for j in range(4):
+        for i in range(n):
+            tmp[i] = y[i] + k[i][j] * c[j]
+        tx = x + c[j] * h
+        _, f[i] = grk(tx, tmp[i])
+        for i in range(n):
+            k[i][j] = h * f[i]
+
+    x = x + h
+    for i in range(n):
+        y[i] = y[i] + (k[i][0] + k[i][3]) / 6 + (k[i][1] + k[i][2]) / 3
+
+    return x, y
+
+
+# MAIN ------------------------------------------------------------
+
+h = 1.0e-3      #刻み
+Nmax = 20000    #計算回数
+step = 200      #表示間隔
+n = 1
+y = [0, 0]
+
+#初期値
+x = 0
+y[0] = 2
+
+gx = []
+gy = []
+
+for i in range(Nmax):
+    x, y = rk4(x, y, n, h)
+    if (i % step == 0):
+        #表示
+        print(x, y[0])
+        gx.append(x)
+        gy.append(y[0])
+
+#グラフ表示
+plt.plot(gx, gy)
+plt.show()
+
+
+#  stop
+#end program main
+
